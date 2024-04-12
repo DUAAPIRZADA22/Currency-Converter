aa41ab7df6d48f9f81c6de71316d78e6d87d4937
Duaa <160533681+DUAAPIRZADA22@users.noreply.github.com>
2024-04-13 03:39:36 +0500

    This is a simple currency converter

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.58,
+    PKR: 280
+};
+let user_ans = await inquirer.prompt([{
+        name: "from",
+        message: "Enter From Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    { name: "to",
+        message: "Enter To Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number"
+    }
+]);
+console.log(user_ans);
+let fromAmount = currency[user_ans.from]; // exchange rate
+let toAmount = currency[user_ans.to]; // exchange rate
+let amount = user_ans.amount;
+let baseAmount = amount / fromAmount; // USD base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
